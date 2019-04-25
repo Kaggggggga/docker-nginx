90b48b4f23080c3e71e3da8e7fe4dcbfaf9cdaa2
kagahehk01 <kagahe@hk01.com>
2019-04-25 13:29:26 +0800

    fix: fix nit-php-fpm-proxy-default.sh not execute

--- scripts/init.d/init-php-fpm-proxy-default.sh
+++ scripts/init.d/init-php-fpm-proxy-default.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-if [ ${NGINX_TEMPLATE} -eq "php-fpm-proxy" ]; then
+if [ ${NGINX_TEMPLATE} = "php-fpm-proxy" ]; then
     echo "init-php-fpm-proxy-default"
+    mkdir -p $NGINX_PHP_ROOT
     touch "$NGINX_PHP_ROOT/index.php"
 fi
