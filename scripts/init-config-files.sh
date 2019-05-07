b9592ec0badcb6f1660ebbc0223d26f02f570a2e
Kaggggggga <kaga.fromearth@gmail.com>
2019-05-08 01:39:58 +0800

    feat: add template for cache-proxy

--- scripts/init-config-files.sh
+++ scripts/init-config-files.sh
@@ -6,7 +6,7 @@ base="/nginx"
 init_scripts="${base}/scripts/init.d/*"
 for script in $init_scripts; do
     if [ -f $script -a -x $script ]; then
-        echo "start init script: ${script}"
+#        echo "start init script: ${script}"
         . $script
     fi
 done
