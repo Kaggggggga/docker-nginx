9eb5931ca1c9957b970330880c578fcf9889e0d6
Kaggggggga <kaga.fromearth@gmail.com>
2019-08-07 02:22:31 +0800

    fix: bash if handling

--- scripts/init.d/init-basic-auth-proxy-default.sh
+++ scripts/init.d/init-basic-auth-proxy-default.sh
@@ -2,6 +2,7 @@
 if [ ${NGINX_TEMPLATE} = "basic-auth-proxy" ]; then
     echo "init-basic-auth-proxy-default"
     echo "$NGINX_BASIC_AUTH_USERNAME:{PLAIN}$NGINX_BASIC_AUTH_PASSWORD" > "$NGINX_BASIC_AUTH_PATH"
-    [[ -z "$NGINX_BASIC_AUTH_REALM" ]] && export NGINX_BASIC_AUTH_REALM='Authentication Required'
-
+    if [[ -z "$NGINX_BASIC_AUTH_REALM" ]]; then
+        export NGINX_BASIC_AUTH_REALM='Authentication Required'
+    fi
 fi
